f249bd75573528a7c8508785690bf2a66190f5b4
rosario <58920070+athena272@users.noreply.github.com>
2025-09-23 13:43:06 -0300

    Update observation.c

--- 01-introduction/006/observation.c
+++ 01-introduction/006/observation.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
     char gender;
 
+    // User enters: m<Enter>
     printf("Enter your gender (f/F m/M): ");
-    scanf("%c", &gender); // usuário digita: m<Enter>
+    scanf(" %c", &gender);
     printf("You typed: '%c'\n", gender);
+
+    char another;
+    // It didn't ask anything; reads the leftover '\n'!
+    printf("Next: '%c'\n", another);
+    scanf(" %c", &another);
 
     return 0;
 }
