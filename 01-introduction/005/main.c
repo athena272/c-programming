0f32b97ba99e1c9a027b667931eda10b8fcf217f
rosario <58920070+athena272@users.noreply.github.com>
2025-09-23 12:22:20 -0300

    Create main.c

---
+++ 01-introduction/005/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+   Lesson 5
+   Reading floating-point numbers
+*/
+
+int main(void) {
+    float number = 3.1415;
+
+    printf("Value of my variable: %.2f\n", number);
+
+    printf("Enter a real number: ");
+    if (scanf("%f", &number) != 1) {
+        fprintf(stderr, "Invalid input.\n");
+        return 1;
+    }
+
+    printf("Value read: %.3f\n", number);
+
+    return 0;
+}
