efe1e7b72bc330fbdb661c079fa4ce195ce5c0bc
rosario <58920070+athena272@users.noreply.github.com>
2025-09-23 13:43:09 -0300

    Create main.c

---
+++ 01-introduction/007/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+   Lesson 7
+   Reading characters
+   function getchar()
+*/
+
+int main(void) {
+    char letter;
+
+    printf("Type a letter: ");
+    letter = getchar();
+
+    printf("Character read: %c\n", letter);
+
+    return 0;
+}
