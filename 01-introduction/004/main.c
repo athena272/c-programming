a943664455fc89d3aa43f833719642246c36c4a7
rosario <58920070+athena272@users.noreply.github.com>
2025-09-23 09:57:12 -0300

    Update main.c

--- 01-introduction/004/main.c
+++ 01-introduction/004/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+/*
+    Lesson 4
+    Reading integer numbers
+*/
+
+int main(void) {
+
+    int value1, value2; // I created variables to store integer values
+
+    // assignment -> assign a value to a variable
+    value1 = 50; // example assignment (will be replaced by user input)
+
+    printf("Enter an integer value: ");
+    scanf("%d", &value1);
+
+    printf("Enter a second integer value: ");
+    scanf("%d", &value2);
+
+    printf("First value: %d\nSecond value: %d\n", value1, value2);
 
     return 0;
 }
