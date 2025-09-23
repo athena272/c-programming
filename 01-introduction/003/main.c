4b3cbebd1060103d4596e3588bfdbd01fced685e
rosario <58920070+athena272@users.noreply.github.com>
2025-09-22 21:58:20 -0300

    Create main.c

---
+++ 01-introduction/003/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 003 system("pause")
+
+int main() {
+
+    // printf is an output function
+    printf("\n-------------------------------\n\n");
+    printf("1 - Login   2 - Register   3 - Print\n");
+    printf("\n-------------------------------\n");
+
+    printf("\nReturned value: %d\n", printf("Good"));
+
+    printf("Press any key to continue\n");
+    // scanf("%c");
+    getchar();
+
+    return 0;
+}
