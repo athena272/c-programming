175c5bf64583dff70f15e78b8361917f21807d68
rosario <58920070+athena272@users.noreply.github.com>
2025-09-22 21:46:00 -0300

    Create main.c

---
+++ 01-introduction/002/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// escape character to skip a line -> \n
+
+int main() {
+
+    // printf is an output function
+    printf("\n-------------------------------\n\n");
+    printf("1 - Login   2 - Register   3 - Print\n");
+    printf("\n-------------------------------\n");
+
+    printf("\nReturned value: %d\n", printf("Good"));
+
+    return 0;
+}
