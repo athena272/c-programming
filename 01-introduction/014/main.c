1e10344092c77c677f7d554eea69c311eca07e2b
rosario <58920070+athena272@users.noreply.github.com>
2025-09-24 16:43:31 -0300

    Create main.c

---
+++ 01-introduction/014/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    char a, b;
+    printf("Type the 1st letter: ");
+    scanf(" %c", &a);
+
+    printf("Type the 2nd letter: ");
+    scanf(" %c", &b);
+
+    printf("1st letter: %c\n2nd letter: %c\n", a, b);
+}
