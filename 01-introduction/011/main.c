4b3625150e9b5091fefb06d9f745f8589c1a145e
rosario <58920070+athena272@users.noreply.github.com>
2025-09-24 16:43:13 -0300

    Create main.c

---
+++ 01-introduction/011/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+   Lesson 11
+   Read gender, age, weight and height
+*/
+
+int main(void)
+{
+    char gender;
+    int age;
+    float weight, height;
+
+    printf("Enter gender (f, F, m or M), age, weight and height:\n");
+    scanf(" %c%d%f%f", &gender, &age, &weight, &height);
+
+    printf("Gender: %c\nAge: %d\nWeight: %.1f\nHeight: %.2f\n",
+           gender, age, weight, height);
+
+    return 0;
+}
