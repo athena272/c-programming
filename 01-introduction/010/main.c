70173ae2064d5b481d600ef4fd2316fa9f3be9e5
rosario <58920070+athena272@users.noreply.github.com>
2025-09-24 16:43:05 -0300

    Create main.c

---
+++ 01-introduction/010/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+   Lesson 10
+   Reading multiple values
+*/
+
+int main(void)
+{
+    int num1, num2, num3;
+
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    printf("Values read: %d, %d and %d\n", num1, num2, num3);
+
+    return 0;
+}
