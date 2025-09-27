fe74bdf27e8db791e999af6664c5ecc570b67b8b
rosario <58920070+athena272@users.noreply.github.com>
2025-09-27 18:36:46 -0300

    Create main.c

---
+++ 01-introduction/022/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+   Lesson 22
+   long modifier for the double type
+   double      -> %f
+   long double -> %Lf
+*/
+
+int main(void)
+{
+    float x = 3.1415f;
+    long double y = 3.1415896587458962548L; // note the 'L' suffix for long double literals
+
+    printf("Value of y: %.21Lf\n", y);
+    __builtin_printf("Value of y: %.21Lf\n", y);
+    printf("A long double requires %zu bytes of memory.\n", sizeof y);
+
+    return 0;
+}
