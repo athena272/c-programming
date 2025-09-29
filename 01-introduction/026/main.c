03bbd02837d9f0aa2aa83bec9bd20356d5e9c1dc
rosario <58920070+athena272@users.noreply.github.com>
2025-09-29 15:36:08 -0300

    Create main.c

---
+++ 01-introduction/026/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+   Lesson 25
+   remainder operator (%)
+*/
+
+int main(void)
+{
+    int a = 7, b = 21;
+
+    printf("Remainder of integer division: %d\n", b % 2);
+
+    return 0;
+}
