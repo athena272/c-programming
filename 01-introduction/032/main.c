a7efb36708360fcb4818f62ab16a5e285388ed43
rosario <58920070+athena272@users.noreply.github.com>
2025-09-30 14:55:48 -0300

    Update main.c

--- 01-introduction/032/main.c
+++ 01-introduction/032/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+  Task:
+  Write a program that receives two values from the keyboard, one for variable 'a'
+  and one for variable 'b'. Then perform the necessary steps so that, at the end,
+  variable 'a' holds the value that was initially in 'b', and variable 'b' holds
+  the value that was initially in 'a'.
+*/
+
 int main()
 {
+    int a, b, aux;
+    printf("Enter two values: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Read values: a = %d | b = %d\n", a, b);
+
+    // Swap using auxiliary variable
+    aux = a;
+    a = b;
+    b = aux;
+
+    printf("Swapped values: a = %d | b = %d\n", a, b);
 
     return 0;
 }
