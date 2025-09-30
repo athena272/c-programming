ab10d77a63b07f8c0dfab644d94f6c18fab358f8
rosario <58920070+athena272@users.noreply.github.com>
2025-09-30 14:40:09 -0300

    Create main.c

---
+++ 01-introduction/028/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+   Lesson 28
+   decrement operator --
+*/
+
+int main(void)
+{
+    int result, counter = 10;
+
+    // synonyms
+    // counter--;
+    // counter -= 1;
+    // counter = counter - 1;
+
+    result = --counter; // pre-decrement
+
+    printf("Value: %d\n", result);
+
+    return 0;
+}
