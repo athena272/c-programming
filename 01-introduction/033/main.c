a7bad8f1ed7fe5855d92bc751f3b1e724f7c8f7d
rosario <58920070+athena272@users.noreply.github.com>
2025-10-01 08:18:54 -0300

    Create main.c

---
+++ 01-introduction/033/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+  Task:
+  Write a C program to swap the values of two integer variables
+  without using any auxiliary variable.
+*/
+
+int main(void) {
+    int a, b;
+
+    printf("Enter two values: ");
+    scanf("%d%d", &a, &b);
+
+    printf("Initial values: a = %d | b = %d\n", a, b);
+
+    /*
+       Example with a = 10 and b = 5
+       a = a + b -> 15
+       b = a - b -> 10
+       a = a - b -> 5
+    */
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("Swapped values: a = %d | b = %d\n", a, b);
+
+    return 0;
+}
