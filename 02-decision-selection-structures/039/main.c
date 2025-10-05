45000aa5925e9363af975a3a48db4ed47c115bab
rosario <58920070+athena272@users.noreply.github.com>
2025-10-05 18:45:01 -0300

    Create main.c

---
+++ 02-decision-selection-structures/039/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+   Lesson 39: Complete decision
+   Checking if a number is negative
+*/
+
+int main() {
+    int a = 10;
+
+    printf("Enter any value: ");
+    scanf("%d", &a);
+
+    if (a < 0)
+        printf("\tNegative value!\n");
+
+    printf("Program continuation...\n");
+
+    return 0;
+}
