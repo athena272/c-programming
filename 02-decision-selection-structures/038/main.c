94e5f9d24edd0872a521731ee8ae9925dfb71d1b
rosario <58920070+athena272@users.noreply.github.com>
2025-10-05 18:12:11 -0300

    Create main.c

---
+++ 02-decision-selection-structures/038/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void print_relations(int a, int b)
+{
+    printf("Comparing a = %d, b = %d\n", a, b);
+    printf("a <  b  : %d\n", a < b);
+    printf("a >  b  : %d\n", a > b);
+    printf("a <= b  : %d\n", a <= b);
+    printf("a >= b  : %d\n", a >= b);
+    printf("a == b  : %d\n", a == b);
+    printf("a != b  : %d\n", a != b);
+}
+
+int main()
+{
+    int a = 10, b = 20;
+
+    // Case 1: a < b
+    print_relations(a, b);
+
+    // Case 2: a == b
+    a = b;
+    print_relations(a, b);
+
+    // Case 3: a > b
+    a = 30;
+    b = 20;
+    print_relations(a, b);
+
+    return 0;
+}
