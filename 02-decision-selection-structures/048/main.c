af5b0f3c7fe29efed755903d495be1cc88e0846a
rosario <58920070+athena272@users.noreply.github.com>
2025-10-08 22:43:53 -0300

    Create main.c

---
+++ 02-decision-selection-structures/048/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+   Lesson 48: Logical OR (||)
+   Truth Table:
+   -----------------
+   a     b   |  a || b
+   true true |  true
+   true false|  true
+   false true|  true
+   false false| false
+*/
+
+int main(void)
+{
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if (age <= 5 || age >= 60)
+        printf("Eligible for free fare!\n");
+    else
+        printf("Not eligible for free fare!\n");
+
+    return 0;
+}
