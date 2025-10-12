d3d64fe43ead94479fea240ab0ea426dbd3bfe65
rosario <58920070+athena272@users.noreply.github.com>
2025-10-11 21:51:18 -0300

    Create main.c

---
+++ 03-repetition-structures-in-c/066/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Class 66: Increment different from 1
+
+    Example: Print all odd numbers from 1 to 100 using a for loop
+    with an increment of 2.
+*/
+
+int main(void)
+{
+    int i;
+
+    for (i = 1; i <= 100; i += 2) // prints 50 numbers (1, 3, 5, ...)
+        printf("%d ", i);
+
+    printf("\n");
+    return EXIT_SUCCESS;
+}
