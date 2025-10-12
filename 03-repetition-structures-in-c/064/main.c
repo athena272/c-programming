8deba0a31446b422d346e27a3aee51039573b93a
rosario <58920070+athena272@users.noreply.github.com>
2025-10-11 21:51:12 -0300

    Create main.c

---
+++ 03-repetition-structures-in-c/064/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Class 64: For loop (repetition structure)
+
+    Demo: print numbers using manual i++ calls (not recommended)
+    and then using a for loop (recommended).
+*/
+
+int main(void)
+{
+    int i = 0;
+
+    // Manual increments (just to illustrate why a loop is better)
+    printf("%d ", i++);
+    printf("%d ", i++);
+    printf("%d ", i++);
+    printf("%d ", i++);
+    printf("%d ", i++);
+    printf("%d ", i++);
+    printf("%d ", i++);
+    printf("%d ", i++);
+    printf("%d ", i++);
+    printf("%d\n", i++);
+
+    // for (init; condition; increment)
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%d ", i);
+        printf("%d | ", 500);
+    }
+    printf("\n");
+
+    return EXIT_SUCCESS;
+}
