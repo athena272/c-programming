a1f87fc2fbaa85cf6429658a6ea42457e90acb3b
rosario <58920070+athena272@users.noreply.github.com>
2025-10-11 21:51:15 -0300

    Create main.c

---
+++ 03-repetition-structures-in-c/065/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Class 65: Decreasing For Loop Structure
+
+    Demonstration: printing numbers in descending order using manual decrements
+    and then using a for loop.
+*/
+
+int main(void)
+{
+    int i = 10;
+
+    // Manual decrements (illustrating the logic behind a descending loop)
+    printf("%d ", i--);
+    printf("%d ", i--);
+    printf("%d ", i--);
+    printf("%d ", i--);
+    printf("%d ", i--);
+    printf("%d ", i--);
+    printf("%d ", i--);
+    printf("%d ", i--);
+    printf("%d ", i--);
+    printf("%d\n", i--);
+
+    // for (initialization; stop condition; decrement)
+    for (i = 10; i >= 0; i--)
+    {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+    return EXIT_SUCCESS;
+}
