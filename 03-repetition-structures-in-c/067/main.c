9bff8a4e32518183a476c25abaa285e4087d0e4b
rosario <58920070+athena272@users.noreply.github.com>
2025-10-11 21:51:21 -0300

    Create main.c

---
+++ 03-repetition-structures-in-c/067/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Class 67: While loop
+
+    Ask the user for a value greater than zero.
+    While the value is not greater than zero, keep asking again.
+*/
+
+int main(void)
+{
+    int value;
+
+    printf("Enter a value greater than zero: ");
+    if (scanf("%d", &value) != 1)
+    {
+        fprintf(stderr, "Invalid input.\n");
+        return EXIT_FAILURE;
+    }
+
+    // while (value <= 0) keep asking
+    while (value <= 0)
+    {
+        printf("Invalid value! Enter a value greater than zero: ");
+        if (scanf("%d", &value) != 1)
+        {
+            fprintf(stderr, "Invalid input.\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    printf("Value read: %d\n", value);
+    return EXIT_SUCCESS;
+}
