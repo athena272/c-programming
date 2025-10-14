8d78ea8bdfb5391960b3d58a0e75ccae039aba57
rosario <58920070+athena272@users.noreply.github.com>
2025-10-14 08:34:51 -0300

    Create main.c

---
+++ 03-repetition-structures-in-c/076/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Class 76: Exercise 07
+
+    Read an integer N. Print the square of each even number from 1 up to N
+    (inclusive), when applicable.
+*/
+
+int main()
+{
+    int i, n;
+
+    printf("Enter an integer N: ");
+    if (scanf("%d", &n) != 1)
+    {
+        fprintf(stderr, "Invalid input.\n");
+        return EXIT_FAILURE;
+    }
+
+    // Start at 2 (first positive even) and jump by 2 to visit only evens
+    for (i = 2; i <= n; i += 2)
+    {
+        printf("%d^2 = %d\n", i, i * i);
+    }
+
+    return EXIT_SUCCESS;
+}
