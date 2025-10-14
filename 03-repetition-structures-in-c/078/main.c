c33e29ab93ef08a38ec74a29af1a10af2b0733f0
rosario <58920070+athena272@users.noreply.github.com>
2025-10-14 08:34:56 -0300

    Create main.c

---
+++ 03-repetition-structures-in-c/078/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Class 78: Exercise 09
+
+    Compute A = 1 + 2 + 3 + ... + n, where n is a positive integer
+    provided by the user (n > 0).
+*/
+
+int main()
+{
+    int i, n, A = 0;
+
+    // read n and validate: must be > 0
+    do
+    {
+        printf("Enter the value of n (n > 0): ");
+        if (scanf("%d", &n) != 1)
+        {
+            fprintf(stderr, "Invalid input.\n");
+            return EXIT_FAILURE;
+        }
+    } while (n < 1);
+
+    // accumulate the sum from 1 to n
+    for (i = 1; i <= n; i++)
+    {
+        A += i;
+    }
+
+    printf("Sum from 1 to %d: %d\n", n, A);
+    return EXIT_SUCCESS;
+}
