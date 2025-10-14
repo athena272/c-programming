65f37a7ce19e71f0c59678405d2a63bde8021a73
rosario <58920070+athena272@users.noreply.github.com>
2025-10-14 07:56:27 -0300

    Update main.c

---
+++ 03-repetition-structures-in-c/075/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Class 75: Exercise 06
+
+    Write a program that prints on the screen all multiples of 7 between 1 and 9999.
+*/
+
+int main()
+{
+    int i;
+
+    for (i = 7; i <= 9999; i += 7)
+    {
+        printf("%4d ", i);
+    }
+
+    return EXIT_SUCCESS;
+}
