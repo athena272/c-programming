152357533f17c6dc3113be94f18ee1b29b802ec4
rosario <58920070+athena272@users.noreply.github.com>
2025-11-18 11:21:18 -0300

    Create main.c

---
+++ 04-homogeneous-data-structures-arrays-and-matrices/110/main.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define ROWS 3
+#define COLUMNS 3
+
+int main()
+{
+    int i, j;
+    int matrix[ROWS][COLUMNS];
+
+    srand(time(NULL)); // Seed the random number generator for reproducibility
+    for (i = 0; i < ROWS; i++)
+    {
+        for (j = 0; j < COLUMNS; j++)
+        {
+            matrix[i][j] = 1 + rand() % 99; // Random integers between 1 and 99
+        }
+    }
+
+    for (i = 0; i < ROWS; i++)
+    {
+        for (j = 0; j < COLUMNS; j++)
+        {
+            printf("%3d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
