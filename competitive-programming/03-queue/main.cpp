cf8e0dac52c21034120fb595b7a61171e1a8067d
Guilherme Rosário Alves <58920070+athena272@users.noreply.github.com>
2025-11-24 12:26:17 -0300

    Add files via upload

---
+++ competitive-programming/03-queue/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    queue<int> line;
+    line.push(4);
+    line.push(7);
+    line.push(9);
+
+    int firstElement = line.front();
+    int lastElement = line.back();
+
+    int length = line.size();
+
+    return 0;
+}
---
+++ competitive-programming/05-dictionary/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    map<string, int> dictionary;
+
+    
+}
